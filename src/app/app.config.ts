42497688af0d091c7e2a425311e2249843b54494
Your Name <your.email@example.com>
2025-11-28 13:06:10 +0530

    feat: enable hash-based routing to avoid server rewrite issues on Vercel/Netlify

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,7 +8,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideClientHydration(),
     provideAnimations()
   ]
